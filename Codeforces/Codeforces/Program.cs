3161a7f90ac786392edea10c12fada852e60d225
Bokhtiar Rifat <mdbrifat@gmail.com>
2023-01-19 17:38:11 +0600

    New file added

--- Codeforces/Codeforces/Program.cs
+++ Codeforces/Codeforces/Program.cs
@@ -1,4 +1,16 @@
 ﻿var firstLine = Console.ReadLine();
 var lastLine = Console.ReadLine();
-
-Console.WriteLine(String.Compare(firstLine,lastLine,StringComparison.OrdinalIgnoreCase));
+Console.WriteLine(CompareStrings(lastLine, firstLine));
+int CompareStrings(string a, string b)
+      { 
+            int result = String.Compare(firstLine, lastLine, StringComparison.OrdinalIgnoreCase);
+    if(result == 0)
+    {
+        return 0;
+    }
+    else if(result > 0)
+    {
+        return 1;
+    }
+    return -1;
+      }
