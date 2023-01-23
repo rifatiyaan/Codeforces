6c8c9679b06d4de4483a345ad452ce28588aa968
Bokhtiar Rifat <mdbrifat@gmail.com>
2023-01-23 19:40:40 +0600

    New code added

--- Codeforces/Forgotten Episode/Program.cs
+++ Codeforces/Forgotten Episode/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
-
 int inputOne = int.Parse(Console.ReadLine());
-int[] array = new int[inputOne];
 
-for (int i = 1; i < inputOne; i++)
+int[] compare = new int[inputOne];
+for (int i = 0;i < compare.Length; i++)
 {
-  array[i] =  Console.ReadLine();
-    
-   
-    if (numbers[i] != i)
-    {
-        Console.WriteLine(i);
-    }
+    compare[i] = i + 1;
+}
+
+string[] array = Console.ReadLine().Split(' ');
+int[] array2 = new int[array.Length];
+
+for (int i = 0; i < inputOne - 1; i++)
+{
+    array2[i] = int.Parse(array[i]);
+}
+
+int[] result = compare.Except(array2).ToArray();
+
+foreach (var item in result)
+{
+    Console.Write(item);
 }
 
