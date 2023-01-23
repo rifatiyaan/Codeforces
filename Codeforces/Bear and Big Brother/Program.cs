8bf3617fbbcefec8bd34793602eedce4df47ac7f
Bokhtiar Rifat <mdbrifat@gmail.com>
2023-01-23 22:28:22 +0600

    Added new code

---
+++ Codeforces/Bear and Big Brother/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] inputs = Console.ReadLine().Split();
+int limak = int.Parse(inputs[0]);
+int bob =int.Parse(inputs[1]);
+int counter = 0;
+for (int i = 1; i < 1000; i++)
+{
+    while (bob >= limak)
+    {
+        limak = i*limak*3;
+        bob = i*bob*2;
+        counter++;
+    }
+}
+Console.WriteLine(counter);
---
+++ Codeforces/Stones on the Table/Program.cs
@@ -0,0 +1,15 @@
+﻿int input = int.Parse(Console.ReadLine());
+int counter = 0;
+string line = Console.ReadLine();
+while (line.Length == input)
+{ 
+	for (int i = 0; i < input - 1; i++)
+	{
+		if (line[i] == line[i + 1])
+		{
+			counter++;
+		}
+	}
+	break;
+}
+Console.WriteLine(counter);
