25d06a2f8f8b3b1f265f67a737d9afa14aa9d134
Bokhtiar Rifat <mdbrifat@gmail.com>
2023-01-25 23:37:05 +0600

    Added new code

---
+++ Codeforces/Football/Program.cs
@@ -0,0 +1,32 @@
+﻿string input = Console.ReadLine();
+
+string[] b = input.Split();
+string[] a = new string[b.Length];
+
+for (int i = 0; i < b.Length; i++)
+{
+    a[i] = b[i];
+}
+foreach (var item in a)
+{
+    Console.WriteLine(item);
+}
+//for(int i = 0;i < a.Length; i++)
+//{
+//    if( a.Length>=7 &&  a[i] == a[i + 1] 
+//        &&a[i] == a[i + 2]
+//        &&a[i] == a[i + 3]
+//        &&a[i] == a[i + 4]
+//        &&a[i] == a[i + 5]
+//        &&a[i] == a[i + 6]
+//        &&a[i] == a[i + 7]
+//       )
+//    {
+//        Console.WriteLine("YES");
+//    }
+//    else 
+//       {
+//        Console.WriteLine("NO");     
+//       }
+    
+//}
---
+++ Codeforces/Word/Program.cs
@@ -0,0 +1,18 @@
+﻿string a = Console.ReadLine();
+
+int up= 0;
+int down= 0;
+
+foreach (var item in a)
+{
+    if (Char.IsUpper(item))
+    {
+        up++;
+    }
+    if (char.IsLower(item))
+    {
+        down++;
+    }
+}
+string result = (up > down) ? $"{a.ToUpper()}" : $"{a.ToLower()}";
+Console.WriteLine(result);
