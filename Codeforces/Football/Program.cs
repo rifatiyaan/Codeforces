e8e461ecfc2317a35b64c375d9da2f42b80d257a
rifatiyaan <mdbrifat@gmail.com>
2023-08-06 16:20:53 +0600

    added more code

--- Codeforces/Football/Program.cs
+++ Codeforces/Football/Program.cs
@@ -1,32 +1,9 @@
-﻿string input = Console.ReadLine();
+﻿var input = Console.ReadLine();
 
-string[] b = input.Split();
-string[] a = new string[b.Length];
+string zeros = "0000000";
+string ones = "1111111";
 
-for (int i = 0; i < b.Length; i++)
-{
-    a[i] = b[i];
-}
-foreach (var item in a)
-{
-    Console.WriteLine(item);
-}
-//for(int i = 0;i < a.Length; i++)
-//{
-//    if( a.Length>=7 &&  a[i] == a[i + 1] 
-//        &&a[i] == a[i + 2]
-//        &&a[i] == a[i + 3]
-//        &&a[i] == a[i + 4]
-//        &&a[i] == a[i + 5]
-//        &&a[i] == a[i + 6]
-//        &&a[i] == a[i + 7]
-//       )
-//    {
-//        Console.WriteLine("YES");
-//    }
-//    else 
-//       {
-//        Console.WriteLine("NO");     
-//       }
-    
-//}
+bool resultOne = input.Contains(zeros) ? true: false;
+bool resulTwo = input.Contains(ones) ? true: false;
+string result = ((resultOne || resulTwo) == true) ? "YES" : "NO";
+Console.WriteLine(result);
