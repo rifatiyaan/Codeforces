187313db0a46e4e2c0eff8dc24ac3152251ad2e5
rifatiyaan <mdbrifat@gmail.com>
2023-09-04 01:57:07 +0600

    added new code

---
+++ Codeforces/A. Arrival of the General/Program.cs
@@ -0,0 +1,14 @@
+﻿int size = int.Parse(Console.ReadLine());
+string input = Console.ReadLine();
+string[] inputArr = input.Split(" ");
+int[] numbers = Array.ConvertAll(inputArr, int.Parse);
+
+if (numbers.Length == size)
+{
+    int maxValueIndex = Array.IndexOf(numbers, numbers.Max());
+    int minValueIndex = Array.LastIndexOf(numbers, numbers.Min());
+    int output = (minValueIndex < maxValueIndex)? 
+        (((size - 1) - minValueIndex) + maxValueIndex) - 1
+        : maxValueIndex + ((size - 1) - minValueIndex);
+    Console.WriteLine(output);
+}
