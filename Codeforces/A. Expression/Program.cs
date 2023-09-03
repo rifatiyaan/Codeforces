d1ac1a79959838e0e98d3f8bc6ba33695a72d85f
rifatiyaan <mdbrifat@gmail.com>
2023-09-04 00:56:55 +0600

    added code

---
+++ Codeforces/A. Expression/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int firstResult = a+(b*c);
+int secondResult = a*b*c ;
+int thirdResult = (a+b)*c ;
+int fourthResult = a * (b + c);
+int fifthResult = a + b + c;
+
+int[] resultArray ={firstResult,secondResult,thirdResult,fourthResult,fifthResult}; 
+
+Console.WriteLine(resultArray.Max());
---
+++ Codeforces/Leetcode/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Leetcode
+{
+    public void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(RemoveElement(numbers, 3));
+
+
+        int RemoveElement(int[] nums, int val)
+        {
+            int current = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[current] = nums[i];
+                    current++;
+                }
+            }
+            return current;
+        }
+    }
+}
