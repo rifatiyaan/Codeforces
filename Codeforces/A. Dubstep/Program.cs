8dabf8b5fe9f6758a7b8c8e0d20b6bb5da1c0c53
rifatiyaan <mdbrifat@gmail.com>
2023-09-22 01:18:17 +0600

    Added code

---
+++ Codeforces/A. Dubstep/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+var input = Console.ReadLine();
+
+string a = "WUB";
+string b = " ";
+
+string[] result = input.Replace(a, b).Trim().
+    Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries);
+StringBuilder sb = new StringBuilder();
+foreach(string s in result)
+{
+    sb.Append(s+" ");
+}
+Console.WriteLine(sb);
